f70efeb5cf8e90f5ca900808e1067ff73187a871
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-02-18 17:05:15 -0600

    Make colored water an infinite supply like vanilla water

--- src/main/java/net/pl3x/colored_water/proxy/ServerProxy.java
+++ src/main/java/net/pl3x/colored_water/proxy/ServerProxy.java
@@ -1,8 +1,15 @@
 package net.pl3x.colored_water.proxy;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.particle.Particle;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.pl3x.colored_water.block.BlockWater;
 import net.pl3x.colored_water.block.ModBlocks;
 import net.pl3x.colored_water.fluid.ModFluids;
 
@@ -13,9 +20,17 @@ public class ServerProxy {
     }
 
     public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     public void drawParticle(Particle particle) {
     }
-}
 
+    @SubscribeEvent
+    public void onCreateFluidSource(BlockEvent.CreateFluidSourceEvent event) {
+        Block block = event.getState().getBlock();
+        if (block instanceof BlockWater && block != Blocks.FLOWING_WATER) {
+            event.setResult(Event.Result.ALLOW);
+        }
+    }
+}
